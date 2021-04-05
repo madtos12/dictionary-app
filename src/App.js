e4e2f0667535ce7f19f4f4e36011b9960335eb24
madtos12 <78381102+madtos12@users.noreply.github.com>
2021-04-04 22:44:54 -0400

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Dictionary App</h1>
+    </div>
+  );
+}
+
+export default App;
