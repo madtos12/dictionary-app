26dc918840d64f5cc83559e2ecb7ee0b8cdbac1e
madtos12 <78381102+madtos12@users.noreply.github.com>
2021-04-04 22:58:38 -0400

    Added footer with github url

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <small>
+        <a
+          href="https://github.com/madtos12/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Madeline Santos
+      </small>
+    </div>
+  );
+}
