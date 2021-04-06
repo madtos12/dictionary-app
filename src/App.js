638572e16130a72c873cdba37b66b0d957996c23
madtos12 <78381102+madtos12@users.noreply.github.com>
2021-04-05 22:47:10 -0400

    Added container and Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Dictionary from "./Dictionary";
 import Footer from "./Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Dictionary App</h1>
-      <Footer />
+      <div className="container">
+        <header className="App-header"></header>
+        <h1>Dictionary App</h1>
+        <Dictionary />
+        <Footer />
+      </div>
     </div>
   );
 }
