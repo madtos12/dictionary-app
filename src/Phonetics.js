dc845ba684f9465dc833451a98e0fcf22aba5868
madtos12 <78381102+madtos12@users.noreply.github.com>
2021-04-17 23:33:22 -0400

    Added phonetic audio function and styling

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
+  function playAudio() {
+    let audio = new Audio(props.phonetic.audio);
+    audio.play();
+  }
+
   return (
     <div className="Phonetics">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <br />
+      <button onClick={playAudio}>
+        <FontAwesomeIcon icon={faVolumeUp} />
+      </button>
       {props.phonetic.text}
     </div>
   );
